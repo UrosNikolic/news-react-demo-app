c004c006b6e0226b79da3e8d50645d7c7f2c6a13
Uros Nikolic <unikolic94@gmail.com>
2019-04-07 04:34:33 +0200

    initial app setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.scss'
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import Article from './components/article/Article'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <div>
+            <Header/>
+            <div className="news-body">
+              <Route exact path="/" component={Home}/>
+              <Route path="/article/:id" component={Article}/>
+              <Route path="/about" component={About}/>
+            </div>
+            <Footer/>
+          </div>
+        </Router>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
